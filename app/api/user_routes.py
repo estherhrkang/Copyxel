e8b086a57a2342374b763763a8283e0dc385fe79
Hye Ri Kang <estherhrkang@gmail.com>
2021-07-27 18:01:48 -0400

    create user routes

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import User
+from app.models import User, db
 
 user_routes = Blueprint('users', __name__)
 
 
+# get all users
 @user_routes.route('/')
 @login_required
 def users():
@@ -12,8 +13,33 @@ def users():
     return {'users': [user.to_dict() for user in users]}
 
 
+# get a specific user
 @user_routes.route('/<int:id>')
 @login_required
 def user(id):
     user = User.query.get(id)
+    return user.to_dict()
+
+
+# update a specific user
+@user_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def updateUser(id):
+    data = request.get_json()
+    user = User.query.get(id)
+    user.username = data['username']
+    user.email = data['email']
+    user.password = data['password']
+    user.profile_img = data['profile_img']
+    db.session.commit()
+    return user.to_dict()
+
+
+# delete a specific user
+@user_routes.route('/<int:id', methods=['DELETE'])
+@login_required
+def deleteUser(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
     return user.to_dict()
