dca65bd96e13394197fe72738307196439df39c1
Hye Ri Kang <estherhrkang@gmail.com>
2021-07-30 13:11:19 -0400

    set up swiper slides and display drawing title and date

--- react-app/src/components/Home/Slide.js
+++ react-app/src/components/Home/Slide.js
@@ -1,10 +1,23 @@
 import styles from '../../css-modules/Slide.module.css';
 
-export default function Slide() {
+export default function Slide({ drawing }) {
+
+    // drawing.date_created -> Fri, 30 Jul 2021 00:00:00 GMT 
+    function changeDateFormat(date) {
+        const dayOfWk = date.slice(0, 4)
+        const day = date.slice(5, 7)
+        const month = date.slice(8, 11)
+        const year = date.slice(12, 16)
+        return `${dayOfWk} ${month} ${day} ${year}`
+    }
 
     return (
         <div>
-            results slide
+            <div>{drawing.title}</div>
+            <div>
+                {/* {drawing.colors} */}
+            </div>
+            <div>{changeDateFormat(drawing.date_created)}</div>
         </div>
     )
 }
--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -31,26 +31,21 @@ export default function Home() {
     // after keying into drawing store obj -> breaks!
     // [ {colors: stringified allColors...}, {colors: stringified allColors...} ... ]
     // const drawingsArray = useSelector(state => Object.values(state.drawing.drawing));
-    // or
+    // --or--
     // const drawingsArray = useSelector(state => state.drawing.drawing['drawings']);
     // console.log('whats in drawings store with drawings key?', drawingsArray);
 
+    console.log('whats in drawingsobj . drawings ?', drawingsObj?.drawings);
+
     const slides = [];
-    // for (let drawing in drawingsObj['drawings']) {
-    //     slides.push(
-    //         <SwiperSlide key={drawing.id}>
-    //             <Slide drawing={drawing}/>
-    //         </SwiperSlide>
-    //     )
-    // }
-    // or
-    // drawingsObj['drawings'].forEach((drawing) => {
-        // slides.push(
-            // <SwiperSlide>
-                // <Slide drawing={drawing} />
-            // </SwiperSlide>
-        // )
-    // })
+    const drawingsArray = drawingsObj?.drawings;
+    for (let i = 0; i < drawingsArray?.length; i++) {
+        slides.push(
+            <SwiperSlide key={i} className={styles.swiperSlide}>
+                <Slide drawing={drawingsArray[i]}/>
+            </SwiperSlide>
+        )
+    }
 
     const handlePlayButton = () => {
         if (user) {
@@ -62,13 +57,10 @@ export default function Home() {
 
     return (
         <div className={styles.home}>
-            <h1>display sliding carousel of drawing results by all users</h1>
-
-                <Swiper slidesPerView={3} keyboard={true} mousewheel={true} spaceBetween={30} 
-                    pagination={{ "clickable": true }} className={styles.swiperContainer}>
-                    {slides}
-                </Swiper>
-
+            <Swiper slidesPerView={3} keyboard={true} mousewheel={true} spaceBetween={30} 
+                pagination={{ "clickable": true }} className={styles.swiperContainer}>
+                {slides}
+            </Swiper>
             <button type='button' onClick={handlePlayButton}>Play?</button>
         </div>
     );
