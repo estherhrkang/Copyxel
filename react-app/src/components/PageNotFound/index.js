442d0758b207a400da37b4161558a112bbab389b
Hye Ri Kang <estherhrkang@gmail.com>
2021-08-05 20:35:18 -0400

    create 404 page

--- react-app/src/components/PageNotFound/index.js
+++ react-app/src/components/PageNotFound/index.js
@@ -1,23 +1,21 @@
+import { useHistory } from 'react-router-dom';
 import styles from '../../css-modules/PageNotFound.module.css';
 
 export default function PageNotFound() {
-    <h1>404! Page does not exist</h1>
-
+    const history = useHistory();
+    
     return(
         <div className={styles.cardContainer}>
             <div className={styles.card}>
                 <div className={styles.card__front}>
+                    404
                     <div className={styles.canvas}>
-                        <div className={styles.pixels}>
-                            ?
-                        </div>
+                        Page Not Found!
                     </div>
                 </div>
-                <div className={styles.card__back}>
+                <div className={styles.card__back}  onClick={() => history.push('/')}>
                     <div className={styles.canvas}>
-                        <div className={styles.pixels}>
-                            !
-                        </div>
+                        Back to Home?
                     </div>
                 </div>
             </div>
